46881445a7b634d3c5e82db95c498d8bdd49fbee
mitianyi <mitianyi@kedacom.com>
2019-05-06 17:05:26 +0800

    init homepage

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,9 +1,76 @@
 import React from 'react'
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+
+const {
+    Header, Content, Footer, Sider
+} = Layout
+const {
+    SubMenu
+} = Menu
 
 class Home extends React.Component {
+    state = {
+        collapsed: false
+    }
+
+    onCollapse = (collapsed) => {
+        this.setState({ collapsed })
+    }
+
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    }
+
     render () {
         return (
-            <div>Home page.</div>
+            <Layout style={{ minHeight: '100vh' }}>
+                <Sider
+                    trigger={null}
+                    collapsible
+                    collapsed={this.state.collapsed}
+                    onCollapse={this.onCollapse}>
+                    <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                        <div className="logo" style={{ height: '32px', background: 'rgba(255,255,255,.2)', margin: '16px' }}></div>
+                        <Menu.Item key="1">
+                            <Icon type="pie-chart" />
+                            <span>Option 1</span>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Icon type="desktop" />
+                            <span>Option 2</span>
+                        </Menu.Item>
+                        <SubMenu
+                            key="sub1"
+                            title={<span><Icon type="user" /><span>User</span></span>}>
+                            <Menu.Item key="3">Tom</Menu.Item>
+                            <Menu.Item key="4">Bill</Menu.Item>
+                            <Menu.Item key="5">Alex</Menu.Item>
+                        </SubMenu>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Header style={{ background: '#fff', padding: 0 }}>
+                        <Icon
+                            style={{ fontSize: '26px', marginLeft: '10px', padding: '0 20px' }}
+                            type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                            onClick={this.toggle} />
+                    </Header>
+                    <Content style={{ margin: '0 16px' }}>
+                        <Breadcrumb style={{ margin: '16px 0' }}>
+                            <Breadcrumb.Item>User</Breadcrumb.Item>
+                            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+                            Bill is a cat.
+                        </div>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>
+                        Ant Design ©2018 React-Admin
+                    </Footer>
+                </Layout>
+            </Layout>
         )
     }
 }
