2824310312a8262000c79008141815d34254f88c
mitianyi <mitianyi@kedacom.com>
2019-05-08 18:12:06 +0800

    添加 react 组件异步加载代码

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import asyncComponent from './AsyncComponent'
 
-const AsyncHome = asyncComponent(() => import('../views/Home/Home'))
-const AsyncUser = asyncComponent(() => import('../views/User/User'))
+const AsyncHome = asyncComponent(() => import(/* webpackChunkName: "Home" */'../views/Home/Home'))
+const AsyncUser = asyncComponent(() => import(/* webpackChunkName: "User" */'../views/User/User'))
 
 export default () => [
     <Route path="/" key="/" component={AsyncHome} exact />,
