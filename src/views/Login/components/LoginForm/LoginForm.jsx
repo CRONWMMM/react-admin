b109e67a6dbe9903db9c081b4c913993fd9c8212
mitianyi <mitianyi@kedacom.com>
2019-05-15 14:02:13 +0800

    登录逻辑编写

--- src/views/Login/components/LoginForm/LoginForm.jsx
+++ src/views/Login/components/LoginForm/LoginForm.jsx
@@ -1,59 +1,91 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-// action
+// components
 import {
     Form,
     // Icon,
     Input,
+    message,
     Button,
     Checkbox
 } from 'antd'
+// actions
 import { updateUserInfo } from '../../../../store/action/userAction'
-// components
 import './LoginForm.less'
 
 class LoginForm extends React.Component {
     static propTypes = {
         className: PropTypes.string,
-        updateUserInfo: PropTypes.func
+        updateUserInfo: PropTypes.func,
+        form: PropTypes.object
     }
 
     constructor(props) {
         super(props)
     }
 
-    login = () => {
+    /**
+     * 登录表单提交
+     * @param e
+     */
+    handleSubmit = e => {
+        e.preventDefault()
+        const { form: { validateFields } } = this.props
+        validateFields((err, values) => {
+            if (err) {
+                message.warning('请按要求填写表单')
+            } else {
+                const { username, password } = values
+                console.log(values)
+                this.login(username, password)
+            }
+        })
+    }
+
+    /**
+     * 登录
+     * @param username
+     * @param password
+     */
+    login = (username, password) => {
         const { updateUserInfo } = this.props
         updateUserInfo({
-            username: 'CRONWMMM',
-            password: 'ooo'
-        });
+            username,
+            password
+        })
     }
 
     render() {
-        const { className } = this.props
+        const { className, form: { getFieldDecorator } } = this.props
         const { Item: FormItem } = Form
 
         return (
-          <Form className={[ 'login-form-component', className ]}>
-            <FormItem>
-              <Input placeholder="用户名" />
+          <Form className={[ 'login-form-component', className ]}
+                onSubmit={this.handleSubmit}>
+            <FormItem label="用户名">
+              {getFieldDecorator('username', {
+                  rules: [{ required: true, message: '请填写用户名' }]
+              })(<Input placeholder="用户名" />)}
             </FormItem>
-            <FormItem>
-              <Input type="password" placeholder="密码" />
+            <FormItem label="密码">
+              {getFieldDecorator('password', {
+                  rules: [{ required: true, message: '请填写密码' }]
+              })(<Input type="password" placeholder="密码" />)}
             </FormItem>
             <FormItem>
               <div className="clear-f">
                 <Checkbox className="float-left">记住密码</Checkbox>
                 <a href="#" className="float-right">忘记密码</a>
               </div>
-              <Button type="primary" htmlType="submit" className="login-btn" onClick={this.login}>登录</Button>
+              <Button type="primary" htmlType="submit" className="login-btn">登录</Button>
             </FormItem>
           </Form>
         )
     }
 }
+
+const LoginFormWrap = Form.create({ name: 'loginForm' })(LoginForm);
 
 const mapStateToProps = (state) => {
     return {
@@ -66,4 +98,4 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(LoginForm)
+export default connect(mapStateToProps, mapDispatchToProps)(LoginFormWrap)
