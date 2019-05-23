03280a2e976db86aae0a76a66878a721e619ef01
mitianyi <mitianyi@kedacom.com>
2019-05-23 14:43:02 +0800

    从 App 主模块中拆分出边栏 SiderMenu 菜单组件

--- src/views/App/App.jsx
+++ src/views/App/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { hot } from 'react-hot-loader/root'
-import { Layout, Menu, Breadcrumb, Icon } from 'antd'
-import { Link } from 'react-router-dom'
+import { Layout, Breadcrumb, Icon } from 'antd'
 // utils
 import { fullScreen, normalScreen } from 'libs/utils';
 // router
@@ -10,14 +9,12 @@ import Routes from '../../router'
 import './App.less'
 // components
 import Avatar from '../../components/Avatar/Avatar'
-import PageTags from "../../components/PageTags/PageTags";
+import SiderMenu from '../../components/SiderMenu/SiderMenu'
+import PageTags from '../../components/PageTags/PageTags'
 
 const {
     Header, Content, Sider
 } = Layout
-const {
-    SubMenu
-} = Menu
 
 class App extends React.Component {
     state = {
@@ -61,53 +58,7 @@ class App extends React.Component {
                 collapsible
                 collapsed={collapsed}
                 onCollapse={this.onCollapse}>
-              <Menu theme="dark" defaultSelectedKeys={['sub1']} mode="inline">
-                <i className="iconfont iconreact app-logo"></i>
-                <Menu.Item key="sub1">
-                  <Link to="/">
-                    <Icon type="home" />
-                    <span>首页</span>
-                  </Link>
-                </Menu.Item>
-                <SubMenu
-                    key="sub2"
-                    title={<span><Icon type="pie-chart" /><span>数据可视化</span></span>}>
-                  <Menu.Item key="sub1-item1">图表</Menu.Item>
-                  <Menu.Item key="sub1-item2">热力图</Menu.Item>
-                </SubMenu>
-                <SubMenu
-                    key="sub3"
-                    title={<span><Icon type="file-done" /><span>案例</span></span>}>
-                  <Menu.Item key="sub2-item1">灭霸的响指</Menu.Item>
-                  <Menu.Item key="sub2-item2">音乐播放器</Menu.Item>
-                </SubMenu>
-                <SubMenu
-                    key="sub4"
-                    title={<span><Icon type="apartment" /><span>组件</span></span>}>
-                  <Menu.Item key="sub3-item1">
-                    <Link to="/components/loading">
-                      <span>loading</span>
-                    </Link>
-                  </Menu.Item>
-                  <Menu.Item key="sub3-item2">图片剪裁</Menu.Item>
-                  <Menu.Item key="sub3-item3">表格</Menu.Item>
-                  <Menu.Item key="sub3-item4">拖拽</Menu.Item>
-                </SubMenu>
-                <Menu.Item key="sub5">
-                  <Icon type="dot-chart" />
-                  <span>数据埋点</span>
-                </Menu.Item>
-                <Menu.Item key="sub6">
-                  <Link to="/user">
-                    <Icon type="user" />
-                    <span>个人中心</span>
-                  </Link>
-                </Menu.Item>
-                <Menu.Item key="sub7">
-                  <Icon type="setting" />
-                  <span>系统设置</span>
-                </Menu.Item>
-              </Menu>
+              <SiderMenu />
             </Sider>
             <Layout>
               <Header className="app-header">
