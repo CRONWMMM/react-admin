8eace67aca84a68f9d7f6b7c5ff4ffa3c63741af
mitianyi <mitianyi@kedacom.com>
2019-06-03 17:40:56 +0800

    为 PicViewer 添加 swiper 滚动插件

--- src/views/PicViewer/PicViewer.jsx
+++ src/views/PicViewer/PicViewer.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { hot } from 'react-hot-loader/root'
+// pluggin
+import 'libs/swiper/js/swiper.min.js'
+import 'libs/swiper/css/swiper.min.css'
 // components
 import PictureViewer from 'components/PictureViewer/PictureViewer'
 // images
@@ -8,15 +11,40 @@ import picture1 from '../../assets/images/banner.jpg'
 import './PicViewer.less'
 
 class PicViewer extends React.Component {
+    componentDidMount() {
+        const Swiper = window.Swiper
+        // Swiper
+        const bannerSwiper = new Swiper('.swiper-container', { // eslint-disable-line
+            slidesPerView: 3,
+            spaceBetween: 30,
+            freeMode: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+        })
+    }
 
     render() {
         return (
           <div className="picture-viewer-page">
             <PictureViewer width="600" height="400">
-              <img src={picture1}
-                   alt="图片"
-                   draggable="false" />
+              <img src={picture1} alt="图片" draggable="false" />
             </PictureViewer>
+            <div className="swiper-container swiper-container-initialized swiper-container-horizontal swiper-container-free-mode">
+              <div className="swiper-wrapper">
+                <div className="swiper-slide swiper-slide-active" style={{width: '620px', marginRight: '30px'}}>Slide 1</div>
+                <div className="swiper-slide swiper-slide-next" style={{width: '620px', marginRight: '30px'}}>Slide 2</div>
+                <div className="swiper-slide" style={{width: '620px', marginRight: '30px'}}>Slide 3</div>
+                <div className="swiper-slide" style={{width: '620px', marginRight: '30px'}}>Slide 4</div>
+                <div className="swiper-slide" style={{width: '620px', marginRight: '30px'}}>Slide 5</div>
+                <div className="swiper-slide" style={{width: '620px', marginRight: '30px'}}>Slide 6</div>
+                <div className="swiper-slide" style={{width: '620px', marginRight: '30px'}}>Slide 7</div>
+                <div className="swiper-slide" style={{width: '620px', marginRight: '30px'}}>Slide 8</div>
+                <div className="swiper-slide" style={{width: '620px', marginRight: '30px'}}>Slide 9</div>
+                <div className="swiper-slide" style={{width: '620px', marginRight: '30px'}}>Slide 10</div>
+              </div>
+            </div>
           </div>
         )
     }
