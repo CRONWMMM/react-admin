5d4ed25ccb244fb1caade4a48f02fc38db4c751a
mitianyi <mitianyi@kedacom.com>
2019-06-06 11:07:50 +0800

    Breadcrumb 面包屑导航根据路由自动生成，遗留问题：React-hot-loader 报错：React-hot-loader: reconciliation failed could not dive into [ * ] while some elements are still present in the tree.

--- src/views/App/App.jsx
+++ src/views/App/App.jsx
@@ -1,22 +1,30 @@
 import React from 'react'
 import { hot } from 'react-hot-loader/root'
+import { withRouter } from 'react-router-dom'
+import PropTypes from 'prop-types'
+// components
 import { Layout, Breadcrumb, Icon } from 'antd'
+import Avatar from 'components/Avatar/Avatar'
+import SiderMenu from 'components/SiderMenu/SiderMenu'
+import PageTags from 'components/PageTags/PageTags'
+
 // utils
-import { fullScreen, normalScreen } from 'libs/utils';
+import { fullScreen, normalScreen, findDeeply } from 'libs/utils';
 // router
-import Routes from '../../router'
+import Routes from 'router/index'
+import routes from 'router/routes'
 // style
 import './App.less'
-// components
-import Avatar from '../../components/Avatar/Avatar'
-import SiderMenu from '../../components/SiderMenu/SiderMenu'
-import PageTags from '../../components/PageTags/PageTags'
 
 const {
     Header, Content, Sider
 } = Layout
 
 class App extends React.Component {
+    static propTypes = {
+        location: PropTypes.object.isRequired
+    }
+
     state = {
         collapsed: false, // 左侧主菜单是否折叠，默认否
         fullScreen: false // 是否是全屏，默认否
@@ -48,6 +56,25 @@ class App extends React.Component {
         });
     }
 
+    /**
+     * 根据路由配置文件生成面包屑导航
+     * @param routes {Array} 路由配置文件
+     */
+    generatorBreadcrumb = (routes) => {
+        const { location: { pathname } } = this.props
+        const pathList = pathname.split('/')
+        const len = pathList.length
+        let result = []
+
+        for (let i = len - 1; i > 0; i--) {
+            const target = findDeeply(routes, route => route.path === pathList.join('/'))
+            target && result.unshift(target)
+            pathList.pop()
+        }
+
+        return result.map(({name, path, meta: { tag }}) => <Breadcrumb.Item key={name || path}>{tag}</Breadcrumb.Item>)
+    }
+
     render () {
         const { collapsed, fullScreen } = this.state
 
@@ -67,8 +94,7 @@ class App extends React.Component {
                         type={collapsed ? 'menu-unfold' : 'menu-fold'}
                         onClick={this.toggleMenu} />
                   <Breadcrumb className="app-breadcrumb">
-                    <Breadcrumb.Item>User</Breadcrumb.Item>
-                    <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    {this.generatorBreadcrumb(routes)}
                   </Breadcrumb>
                 </div>
                 {/* right part */}
@@ -87,4 +113,4 @@ class App extends React.Component {
     }
 }
 
-export default hot(App)
+export default hot(withRouter(App))
