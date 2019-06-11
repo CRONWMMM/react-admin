19a6d585233e7b6212e6af470c1bceb5ebad477b
mitianyi <mitianyi@kedacom.com>
2019-06-11 17:24:37 +0800

    UserInfo 模块编写

---
+++ src/views/User/components/EditableField/EditableField.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+// components
+import { Icon, Input } from 'antd';
+// style
+import './EditableField.less'
+
+class EditableField extends React.Component {
+    static propTypes = {
+        icon: PropTypes.string, // icon-type
+        editable: PropTypes.bool, // editable or not
+        children: PropTypes.string.isRequired, // slot 插槽
+        onChange: PropTypes.func // change-handler
+    }
+
+    static defaultProps = {
+        editable: true
+    }
+
+    state = {
+        active: false
+    }
+
+    constructor() {
+        super()
+        this.inputRef = React.createRef()
+    }
+
+    componentDidUpdate() {
+        const { active } = this.state
+        active && this.inputRef.current.focus()
+    }
+
+    handleBlur = () => {
+        this.setState({
+            active: false
+        })
+    }
+
+    handleEnter = () => {
+        this.setState({
+            active: false
+        })
+    }
+
+    handleChange = (e) => {
+        const { onChange } = this.props
+        onChange && onChange(e.currentTarget.value)
+    }
+
+    handleEditBtnClick = () => {
+        this.setState({
+            active: true
+        })
+    }
+
+    render() {
+        const { active } = this.state
+        const { icon, children } = this.props
+
+        /* eslint-disable */
+        return (
+            <div className="editable-field">
+                { icon ? <Icon className="field-icon" type={icon} /> : null }
+                {
+                    active ?
+                        (<Input className="input"
+                                ref={this.inputRef}
+                                value={children}
+                                onBlur={this.handleBlur}
+                                onPressEnter={this.handleEnter}
+                                onChange={this.handleChange} />) :
+                        (<span className="field">
+                          <span className="text">{children}</span>
+                          <Icon type="form" className="btn-icon" onClick={this.handleEditBtnClick} />
+                         </span>)
+                }
+            </div>
+        )
+    }
+}
+
+export default EditableField
--- src/views/User/subPages/UserInfo/UserInfo.jsx
+++ src/views/User/subPages/UserInfo/UserInfo.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Typography, Descriptions, Divider, Icon } from 'antd'
+import { Typography, Descriptions, Divider } from 'antd'
 import { hot } from 'react-hot-loader/root'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 // components
 import AvatarFrame from '../../components/AvatarFrame/AvatarFrame'
+import EditableField from '../../components/EditableField/EditableField'
 // actions
 import { updateUserInfo } from '../../../../store/action/userAction'
 // style
@@ -18,12 +19,23 @@ class UserInfo extends React.Component {
         resetUserName: PropTypes.func.isRequired
     }
 
+    state = {
+        value: 'hello world.'
+    }
+
     onChange = str => {
         const { resetUserName } = this.props
         resetUserName(str)
     }
 
+    handleChange = (value) => {
+        this.setState({
+            value
+        })
+    }
+
     render() {
+        const { value } = this.state
         let { username } = this.props
         username = username || '不写 bug 的米公子'
 
@@ -31,7 +43,7 @@ class UserInfo extends React.Component {
           <div className="user-info-page">
             <div className="left-part">
               <AvatarFrame className="avatar-frame" type="rect" />
-              <Icon type="github" />
+              <EditableField icon="github" onChange={this.handleChange}>{value}</EditableField>
             </div>
             <div className="right-part">
               <Descriptions title="个人信息" border>
